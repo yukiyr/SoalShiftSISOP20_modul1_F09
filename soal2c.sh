5cebf3c9abb325a49252b356fd2226f3c21a7c80
yukiyr <37041284+yukiyr@users.noreply.github.com>
2020-02-21 07:56:14 +0000

    Update soal2c.sh

--- soal2c.sh
+++ soal2c.sh
@@ -3,12 +3,15 @@
 hurufkecil=(a b c d e f g h i j k l m n o p q r s t u v w x y z)
 hurufbesar=(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
 
+abjad=aabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz
+abjad2=AABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ
+
 hour=$1
 jam="$( date +"%H" )"
 
-bawah=${hurufkecil[$jam]}
-atas=${hurufbesar[$jam]}
+bawah="${hurufkecil[$jam]}"
+atas="${hurufbesar[$jam]}"
 
-echo "$hour" | tr "[$bawah-za-$bawah]" '[a-z]' | tr "[$atas-ZA-$atas]" '[A-Z]' > decryptt.txt
+echo "$hour" | tr "[$bawah-za-$bawah]" "${abjad:0:28}" | tr "[$atas-ZA-$atas]" "${abjad2:0:28}" > decryptt.txt
 value=$(<decryptt.txt)
 mv "$hour.txt" "$value.txt"
